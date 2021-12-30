0fdf9dde3ccde2a0a76ec6955640cd64a7c0c39b
AJ Quick <ajquick@gmail.com>
2021-12-29 19:43:30 -0700

    Update app.js
    Change rate of increase to more closely line up with current estimates.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ window.onload = function () {
 function upTime(countTo) {
   now = new Date();
   countTo = new Date(countTo);
-  milliseconds = Math.abs(countTo - now) * 0.0025 + 5200000;
+  //milliseconds = Math.abs(countTo - now) * 0.0025 + 5200000; //Prior to Dec 28th
+  milliseconds = Math.abs(countTo - now) * 0.0016975 + 5200000; //New Dec 28th
   hours = milliseconds / 36e5;
   document.getElementById('float').firstChild.nodeValue = milliseconds.toLocaleString("en", {
     minimumFractionDigits: 0,
